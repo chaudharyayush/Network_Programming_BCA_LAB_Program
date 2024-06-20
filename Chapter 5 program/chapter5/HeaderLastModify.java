a051ca77d7419912c13ad5aa0d705a6ac4a15108
Ayush Chaudhary <ac84a82@gmail.com>
2024-06-20 07:16:24 +0545

    HeaderLastModify_update

--- Chapter 5 program/chapter5/HeaderLastModify.java
+++ Chapter 5 program/chapter5/HeaderLastModify.java
@@ -7,18 +7,34 @@ import java.util.*;
 
 public class HeaderLastModify {
 	public static void main(String[] args) {
-		String url= "http://www.ibiblio.org/xml/";
-			try {
-				URL u = new URL(url);
-				HttpURLConnection http = (HttpURLConnection) u.openConnection();
-				http.setRequestMethod("HEAD");
-				System.out.println(u + " was last modified at " + new Date(http.getLastModified()));
-			} catch (MalformedURLException ex) {
-				System.err.println(url + " is not a URL I understand");
-			} catch (IOException ex) {
-				System.err.println(ex);
+		// The URL to check for the last modified date
+		String url = "http://www.ibiblio.org/xml/";
+
+		try {
+			// Create a URL object from the given string
+			URL u = new URL(url);
+
+			// Open a connection to the URL and cast it to HttpURLConnection
+			HttpURLConnection http = (HttpURLConnection) u.openConnection();
+
+			// Set the request method to HEAD to fetch only the headers
+			http.setRequestMethod("HEAD");
+
+			// Get the last modified date in milliseconds since epoch
+			long lastModified = http.getLastModified();
+
+			// Check if the last modified date is valid
+			if (lastModified == 0) {
+				System.out.println("No last-modified information.");
+			} else {
+				// Print the last modified date as a Date object
+				System.out.println(u + " was last modified at " + new Date(lastModified));
 			}
-			System.out.println();
+		} catch (MalformedURLException ex) {
+			System.err.println(url + " is not a URL I understand");
+		} catch (IOException ex) {
+			System.err.println(ex);
 		}
-	
+		System.out.println();
+	}
 }
