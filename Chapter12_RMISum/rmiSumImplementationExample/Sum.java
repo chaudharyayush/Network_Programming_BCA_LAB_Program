c11606cc2f1c07c9c20bb006ef2f6b8fcf3b8898
Ayush Chaudhary <ac84a82@gmail.com>
2024-08-08 20:21:56 +0545

    Update code

--- Chapter12_RMISum/rmiSumImplementationExample/Sum.java
+++ Chapter12_RMISum/rmiSumImplementationExample/Sum.java
@@ -5,14 +5,18 @@ package rmiSumImplementationExample;
 import java.rmi.*;
 import java.rmi.server.*;
 
+//extends UnicastRemoteObject: Sum extends UnicastRemoteObject, which is a class in the RMI library that provides the necessary functionality for remote objects. 
+//This means that Sum is a remote object that can be exported for remote method calls.
+//implements SumInterface: This specifies that Sum implements the SumInterface interface. This means that Sum must provide an implementation for all methods defined in SumInterface.
+
 public class Sum extends UnicastRemoteObject implements SumInterface {
 
+	//This is the constructor for the Sum class. It throws RemoteException because it is a remote object.
 	protected Sum() throws RemoteException {
-		super();
-		// TODO Auto-generated constructor stub
+		super();// Calls the constructor of the superclass UnicastRemoteObject. This is necessary to properly initialize the remote object.
 	}
 
-	@Override
+	@Override //This annotation indicates that the method sum overrides a method declared in the SumInterface interface.
 	public int sum(int a, int b) throws RemoteException {
 		return a + b;
 	}
--- Chapter12_RMISum/rmiSumImplementationExample/SumServer.java
+++ Chapter12_RMISum/rmiSumImplementationExample/SumServer.java
@@ -2,19 +2,23 @@ package rmiSumImplementationExample;
 
 
 
-import java.rmi.registry.Registry;
-import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;//This imports the Registry interface from the java.rmi.registry package. The Registry interface is used to interact with the RMI registry, where remote objects are registered and looked up.
+import java.rmi.registry.LocateRegistry;//The LocateRegistry class provides methods to create or locate the RMI registry.
 
 public class SumServer {
     public static void main(String[] args) {
         try {
-            // Create an instance of the remote object
+        	
+        	//This line creates an instance of the Sum class, which is a remote object that implements the SumInterface. 
+        	//This object will be registered with the RMI registry so that clients can invoke methods on it remotely.
             Sum robj = new Sum();
 
             // Start the RMI registry on port 1099
             Registry registry = LocateRegistry.createRegistry(1999);
 
-            // Bind the remote object to the registry
+            //This line binds the Sum object (robj) to the RMI registry under the name "Sum Service". 
+            //The rebind method is used to either bind a new name or update an existing binding. 
+            //This allows clients to look up the Sum object using the name "Sum Service".
             registry.rebind("Sum Service", robj);
 
             System.out.println("Server is running...");
